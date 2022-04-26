a53646367948df9480d84abf2ee1801be9dbb90e
Michael J. Rimmer <mjrimmer@qinetiq.com>
2022-04-26 15:25:56 +0100

    Added authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>             // this code is in the frontend code but links to the backend?   
+      {({ signOut, user }) => ( // what is this structure? how does authenticator make these variables available?
+        <div className="App"> 
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
